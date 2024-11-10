e8e94dcc90452aec4b0aa4f21d52fe325d05f6c6
ASzeto99910 <83287704+AidanS99910@users.noreply.github.com>
2024-11-10 14:54:07 -0500

    added js comments

--- bgColor.js
+++ bgColor.js
@@ -1,12 +1,16 @@
 const main = document.querySelector("main");
+const btn = document.querySelector("#color-changer");
 
+// getRandomRgb: Nothing -> Number
+// Produces a random real number between 0 and 255
 function getRandomRgb() {
   return (Math.floor(255 * Math.random()));
 }
-
+// changeBackgroundColor: Color -> Color
+// Changes the background color of main to a random RGB
 function changeBackgroundColor() {
   main.style.backgroundColor = "rgb(" + getRandomRgb() + "," + getRandomRgb() + "," + getRandomRgb() + ")";
 }
 
-const btn = document.querySelector("#color-changer");
+// Calls changeBackgroundColor when clicked
 btn.onclick = changeBackgroundColor;
