1a3f2a81a3a7c4ca89e571bf7bb9b7f2ebb78001
ASzeto99910 <83287704+AidanS99910@users.noreply.github.com>
2024-11-13 21:47:22 -0500

    Added Color gradient and altered randomize color button to reflect this change

--- bgColor.js
+++ bgColor.js
@@ -1,16 +1,40 @@
 const main = document.querySelector("main");
 const btn = document.querySelector("#color-changer");
 
-// getRandomRgb: Nothing -> Number
-// Produces a random real number between 0 and 255
-function getRandomRgb() {
+// getSingleRandomRgb: Nothing -> Number
+// Produces a set of 3 Integers between 0 and 255 (inclusive)
+function getSingleRandomRgb() {
   return (Math.floor(255 * Math.random()));
 }
-// changeBackgroundColor: Color -> Color
-// Changes the background color of main to a random RGB
-function changeBackgroundColor() {
-  main.style.backgroundColor = "rgb(" + getRandomRgb() + "," + getRandomRgb() + "," + getRandomRgb() + ")";
+
+// getFullRandomRgb: Nothing -> String
+// Produces a string which represents an RGB in string form
+function getFullRandomRgb() {
+  let rgb = "rgb("
+  for (let i=0; i<3; i++) {
+    if (i == 2) {
+      rgb = rgb + getSingleRandomRgb() + ")"
+    }
+    else {
+      rgb = rgb + getSingleRandomRgb() + ","
+    }
+  };
+  return rgb;
+}
+
+// changeBackgroundGradient : Nothing -> String
+// Changes the background gradient on the page to 3 random colors
+function changeBackgroundGradient() {
+  let grads = "linear-gradient("
+  for (let i = 0; i < 3; i++)
+  if (i == 2) {
+    grads = grads + getFullRandomRgb() + ")"
+  }
+  else {
+    grads = grads + getFullRandomRgb() + ","
+  };
+  main.style.backgroundImage = grads;
 }
 
 // Calls changeBackgroundColor when clicked
-btn.onclick = changeBackgroundColor;
+btn.onclick = changeBackgroundGradient;
